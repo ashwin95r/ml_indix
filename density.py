c0dbf7b2826566f1dd75c8c6e9aef2240cc7f5c6
ashwin95r <ashwin2007ray@gmail.com>
2014-06-25 11:19:42 +0530

    4

--- density.py
+++ density.py
@@ -6,13 +6,14 @@ a=[]
 i=0
 bucket=0
 for line in f:
-	a.append(int(line.strip()))
+	a.append(int(line.strip())-1)
+
 for line in g:
 	bucket+=1
 	w=int(line.strip().split(',')[0][1:])
 	if(w in a):
 		i+=1
-	if(bucket>=2000):
+	if(bucket==2000):
 		print(i)
 		i=0
 		bucket=0
